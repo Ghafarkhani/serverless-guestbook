7692e37e712a43c154e52c64fc7491587b9eb5b9
غفارخانی <32286092+Ghafarkhani@users.noreply.github.com>
2022-02-17 15:35:12 -0500

    update apiUrl

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://091b4a39.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
